639fb20b2e08d585e566c03b5bff675bbb9567be
Rehnuma <rehnuma.reza11@gmail.com>
2022-04-22 00:05:40 +0600

    hard reset to previous NoteApp

--- NotesApp/AppDelegate.swift
+++ NotesApp/AppDelegate.swift
@@ -14,6 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            navBarAppearance.backgroundColor = UIColor.black
+            UINavigationBar.appearance().standardAppearance = navBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        } else{
+            UINavigationBar.appearance().barTintColor = UIColor.black
+            UINavigationBar.appearance().tintColor = .white
+        }
         return true
     }
 
